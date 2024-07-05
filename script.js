577043dc2004990d4682b126da7c0628305a7f48
Isaac Davidson <isaac.davidson@virginaustralia.com>
2024-07-05 15:17:01 +1000

    Display refreshes after symbol click
    Display will refresh after symbol click, and will display the new
    numbers being clicked.
    
    First number is stored in num1, second in num2, and operator in
    operator.
    
    Need to amend so that the operation occurs on each symbol click (if
    applicable), so that clicking 1, 1, + displays 2.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let calc = {
   display: '',
   num1: null,
   num2: null,
-  operator: null
+  operator: null,
 }
 
 function operation(num1, num2, operator) {
@@ -28,38 +28,80 @@ function operation(num1, num2, operator) {
   switch (operator) {
     case '+':
       result = add(num1, num2);
+      console.log(result);
       break;
     case '-':
       result = subtract(num1, num2);
       break;
-    case '*':
+    case 'x':
       result = multiply(num1, num2);
       break;
-    case '/':
+    case '÷':
       result = divide(num1, num2);
       break;
+    case '=':
+      console.log(result)
     default:
       alert(`${num1}, ${num2}, or ${operator} are invalid, please try again.`)
   }
+  calc = {
+    display: result,
+    num1: null,
+    num2: null,
+    operator: null
+  }
   return result;
+
 }
 
 
+function refreshDisplay(numToDisplay) {
+  const display = document.querySelector("#display")
+  display.textContent = numToDisplay;
+  calc.display = '';
+}
 
 function displayNumber(num) {
   const display = document.querySelector("#display")
   display.textContent = num;
 }
 
-const buttons = document.querySelectorAll(".button");
+const numberButtons = document.querySelectorAll(".button.number");
+const symbolButtons = document.querySelectorAll(".button.symbol");
 
-
-buttons.forEach((button) => {
+numberButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
-
     calc.display += event.target.value;
     displayNumber(calc.display);
+  })
+})
 
-  });
-});
+symbolButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    calc.operator = event.target.value;
+    if (!calc.num1) {
+      calc.num1 = Number(calc.display);
+      refreshDisplay(calc.num1);
+    } else if (calc.num1) {
+      calc.num2 = Number(calc.display);
+      refreshDisplay(calc.num2);
+    }
+
+
+  })
+})
+
+
+//
+// const buttons = document.querySelectorAll(".button");
+//
+//
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//
+//     calc.display += event.target.value;
+//     displayNumber(calc.display);
+//
+//   });
+// });
 
